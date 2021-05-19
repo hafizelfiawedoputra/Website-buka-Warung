6542ba76fe6fc18000f0f6717c22512de2cd888d
Hafiz Elfia Wedo Putra <52981264+hafizelfiawedoputra@users.noreply.github.com>
2021-05-19 12:19:23 +0700

    Delete bukawarung directory

--- bukawarung/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hostname = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname   = 'db_bukawarung';
-    
-    $conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('Sorry Error, No database');
-?>
